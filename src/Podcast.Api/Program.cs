cd28e7755d0704f81b6a7d803c908bb2d25f3363
satish <satish860@gmail.com>
2022-05-13 15:34:31 +0530

    Removed Project ID hard coding

--- src/Podcast.Api/Program.cs
+++ src/Podcast.Api/Program.cs
@@ -1,4 +1,5 @@
 global using FastEndpoints;
+using Google.Apis.Auth.OAuth2;
 using Google.Cloud.Firestore;
 using Podcast.Api.Genre;
 
@@ -8,9 +9,28 @@ var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 
 var url = $"http://0.0.0.0:{port}";
 
+static string GetProjectId()
+{
+    GoogleCredential googleCredential = Google.Apis.Auth.OAuth2
+        .GoogleCredential.GetApplicationDefault();
+    if (googleCredential != null)
+    {
+        ICredential credential = googleCredential.UnderlyingCredential;
+        ServiceAccountCredential serviceAccountCredential =
+            credential as ServiceAccountCredential;
+        if (serviceAccountCredential != null)
+        {
+            return serviceAccountCredential.ProjectId;
+        }
+    }
+    return Google.Api.Gax.Platform.Instance().ProjectId;
+}
+
+var projectId = GetProjectId();
+
 builder.Services.AddFastEndpoints();
 
-builder.Services.AddSingleton<FirestoreDb>(FirestoreDb.Create("itunes-dev"));
+builder.Services.AddSingleton<FirestoreDb>(FirestoreDb.Create(projectId));
 builder.Services.AddScoped<IScrapGenre,ScrapGenre>();
 builder.Services.AddScoped<IGenreRepository,GenreRepository>();
 
